7c9f01e38236ba07b140dc4b4dbab8462f52b152
Guillaume Lagrange <guillaume@glagrange.eu>
2025-02-14 15:40:01 +0100

    feat: add flake for cpp development

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "A minimal C++ development environment using flake-utils";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # Common build inputs for both shells
+        commonBuildInputs = with pkgs; [
+          gcc
+          cmake
+          pkg-config
+        ];
+
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = commonBuildInputs;
+            shellHook = ''
+              echo "Welcome to the C++ development environment!"
+            '';
+          };
+
+          lsp = pkgs.mkShell {
+            buildInputs =
+              with pkgs;
+              [
+                gersemi
+                clang-tools
+              ]
+              ++ commonBuildInputs;
+            shellHook = ''
+              export CMAKE_EXPORT_COMPILE_COMMANDS=ON
+              echo "Welcome to the C++ development environment with LSP tools!"
+            '';
+          };
+        };
+      }
+    );
+}
