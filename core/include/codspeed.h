25eae75ebfd298db1c3f3bed3b71c7b939e2bdb1
Guillaume Lagrange <guillaume@glagrange.eu>
2025-02-19 17:18:44 +0100

    feat(core): allow measurement through codspeed core library

--- core/include/codspeed.h
+++ core/include/codspeed.h
@@ -1,6 +1,25 @@
 #ifndef CODSPEED_H
 #define CODSPEED_H
 
-void hello_codspeed();
+#include <string>
+#include <vector>
+
+class CodSpeed {
+public:
+  // Constructor
+  CodSpeed();
+
+  // Member functions
+  void push_group(const std::string &group);
+  void pop_group();
+  void start_benchmark(const std::string &name);
+  void end_benchmark();
+
+private:
+  std::vector<std::string> benchmarked;
+  std::string current_benchmark;
+  std::vector<std::string> group_stack;
+  bool is_instrumented;
+};
 
 #endif // CODSPEED_H
---
+++ core/src/measurement.hpp
@@ -0,0 +1,32 @@
+#ifndef MEASUREMENT_H
+#define MEASUREMENT_H
+
+#include "callgrind.h"
+#include <string>
+
+inline std::string get_version() {
+#ifdef CODSPEED_VERSION
+  return {CODSPEED_VERSION};
+#else
+  return {""};
+#endif
+}
+
+inline bool measurement_is_instrumented() { return RUNNING_ON_VALGRIND; }
+
+inline void measurement_set_metadata() {
+  std::string metadata = "Metadata: codspeed-cpp " + get_version();
+  CALLGRIND_DUMP_STATS_AT(metadata.c_str());
+}
+
+inline void measurement_start() {
+  CALLGRIND_ZERO_STATS;
+  CALLGRIND_START_INSTRUMENTATION;
+}
+
+inline void measurement_stop(const std::string &name) {
+  CALLGRIND_STOP_INSTRUMENTATION;
+  CALLGRIND_DUMP_STATS_AT(name.c_str());
+};
+
+#endif // MEASUREMENT_H
