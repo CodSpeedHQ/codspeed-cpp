c2668c354233aa67e827487688873d237d82d20b
not-matthias <matthias@codspeed.io>
2025-03-13 12:14:46 +0100

    feat(google_benchmark): add tests

---
+++ core/test/uri.cpp
@@ -0,0 +1,30 @@
+
+#include <gtest/gtest.h>
+#include "codspeed.h"
+
+// Manual definition (to avoid including it in the public header):
+std::string extract_namespace_clang(const std::string& func_str); 
+std::string extract_namespace_gcc(const std::string& func_str);
+
+TEST(UriTest, TestExtractNamespaceClang) {
+    EXPECT_EQ(extract_namespace_clang("auto outer::test12::(anonymous class)::operator()() const"), "outer::test12::");
+    EXPECT_EQ(extract_namespace_clang("auto outer::(anonymous namespace)::test12::(anonymous class)::operator()() const"), "outer::(anonymous namespace)::test12::");
+}
+
+TEST(UriTest, TestExtractNamespaceGcc) {
+    EXPECT_EQ(extract_namespace_gcc("outer::test12::<lambda()>"), "outer::test12::");
+    EXPECT_EQ(extract_namespace_gcc("outer::(anonymous namespace)::test12::<lambda()>"), "outer::(anonymous namespace)::test12::");
+}
+
+
+namespace a {
+namespace b {
+namespace c {
+static std::string pretty_func = ([]() { return __PRETTY_FUNCTION__; })();
+
+TEST(UriTest, TestExtractNamespace) {
+    EXPECT_EQ(extract_lambda_namespace(pretty_func), "a::b::c::");
+}
+}
+}
+}
