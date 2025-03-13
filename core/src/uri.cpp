73100dab591d6871b8a3601e1ce9722a973d08f8
not-matthias <matthias@codspeed.io>
2025-03-13 12:14:46 +0100

    feat(google_benchmark): add tests

--- core/src/uri.cpp
+++ core/src/uri.cpp
@@ -2,6 +2,36 @@
 #include <string>
 #include <iostream>
 
+// Example: auto outer::test12::(anonymous class)::operator()() const
+// Returns: outer::test12::
+std::string extract_namespace_clang(const std::string& pretty_func) {
+  std::size_t anon_class_pos = pretty_func.find("::(anonymous class)");
+  std::size_t space_pos = pretty_func.find(' ');
+
+  if (space_pos == std::string::npos || anon_class_pos == std::string::npos) {
+    return {};
+  }
+  space_pos += 1; // Skip the space
+
+  return pretty_func.substr(space_pos, anon_class_pos - space_pos) + "::";
+}
+
+// Example: outer::test12::<lambda()>
+// Returns: outer::test12::
+std::string extract_namespace_gcc(const std::string& pretty_func) {
+    auto lambda_pos = pretty_func.find("::<lambda()>");
+    if (lambda_pos == std::string::npos) {
+        return {};
+    }
+
+    return pretty_func.substr(0, lambda_pos) + "::";
+}
+
+// Has to pass the pretty function from a lambda: 
+//   (([]() { return __PRETTY_FUNCTION__; })())
+//
+// Returns: An empty string if the namespace could not be extracted, 
+//         otherwise the namespace with a trailing "::"
 std::string extract_lambda_namespace(const std::string& pretty_func) {
   if (pretty_func.find("(anonymous namespace)") != std::string::npos) {
     std::cerr << "[ERROR] Anonymous namespace not supported in " << pretty_func << std::endl;
@@ -9,25 +39,9 @@ std::string extract_lambda_namespace(const std::string& pretty_func) {
   }
 
 #ifdef __clang__
-  // Example: auto outer::test12::(anonymous class)::operator()() const
-
-  std::size_t anon_class_pos = pretty_func.find("(anonymous class)");
-  std::size_t space_pos = pretty_func.find(' ');
-
-  if (space_pos == std::string::npos || anon_class_pos == std::string::npos) {
-    return {};
-  }
-
-  return pretty_func.substr(space_pos, anon_class_pos - space_pos) + "::";
+    return extract_namespace_clang(pretty_func);
 #elif __GNUC__
-    // Example: outer::test12::<lambda()>
-
-    auto lambda_pos = pretty_func.find("::<lambda()>");
-    if (lambda_pos == std::string::npos) {
-        return {};
-    }
-
-    return pretty_func.substr(0, lambda_pos) + "::";
+    return extract_namespace_gcc(pretty_func);
 #else
 #error "Unsupported compiler"
 #endif
